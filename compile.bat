794b300a5578d4a57e6d85a04584ba186eade1e9
Eric Ribeiro <eric.ribeiro@live.com>
2020-10-28 23:23:37 -0500

    Updated all files to the most recent ones

--- compile.bat
+++ compile.bat
@@ -5,25 +5,25 @@ cd E:\QuakeClassStuff\quake-leveldesign-starterkit-master
 
 
 echo Copying Files...
-copy E:\QuakeClassStuff\quake-leveldesign-starterkit-master\Id1\maps\ZombieMap.map E:\QuakeClassStuff\quake-leveldesign-starterkit-master
+copy E:\QuakeClassStuff\quake-leveldesign-starterkit-master\Id1\maps\DevilsLab.map E:\QuakeClassStuff\quake-leveldesign-starterkit-master
 
 
 echo Converting map...
 
 
 echo --------------QBSP--------------
-E:\QuakeClassStuff\quake-leveldesign-starterkit-master\tools\ericw-tools\bin\qbsp.exe ZombieMap
+E:\QuakeClassStuff\quake-leveldesign-starterkit-master\tools\ericw-tools\bin\qbsp.exe DevilsLab
 
 echo --------------VIS---------------
-E:\QuakeClassStuff\quake-leveldesign-starterkit-master\tools\ericw-tools\bin\vis.exe ZombieMap
+E:\QuakeClassStuff\quake-leveldesign-starterkit-master\tools\ericw-tools\bin\vis.exe DevilsLab
 
 echo -------------LIGHT--------------
-E:\QuakeClassStuff\quake-leveldesign-starterkit-master\tools\ericw-tools\bin\light.exe ZombieMap
+E:\QuakeClassStuff\quake-leveldesign-starterkit-master\tools\ericw-tools\bin\light.exe DevilsLab
 
-copy ZombieMap.bsp E:\QuakeClassStuff\quake-leveldesign-starterkit-master\Id1\maps
-copy ZombieMap.pts E:\QuakeClassStuff\quake-leveldesign-starterkit-master\Id1\maps
-copy ZombieMap.lit E:\QuakeClassStuff\quake-leveldesign-starterkit-master\Id1\maps
+copy DevilsLab.bsp E:\QuakeClassStuff\quake-leveldesign-starterkit-master\Id1\maps
+copy DevilsLab.pts E:\QuakeClassStuff\quake-leveldesign-starterkit-master\Id1\maps
+copy DevilsLab.lit E:\QuakeClassStuff\quake-leveldesign-starterkit-master\Id1\maps
 pause
 cd\
 cd E:\QuakeClassStuff\quake-leveldesign-starterkit-master
-quakespasm  +map ZombieMap
+quakespasm  +map DevilsLab
